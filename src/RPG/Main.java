64844c29361a36821e1da95cbc8498244a3f8415
ryogo <>
2020-11-06 23:46:22 +0900

    11/6 main へっぽこ追加

--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Hero h = new Hero();
 		Battle b = new Battle();
-		//冒険（茶番）の始まりだじょー 1234!!
+		//冒険（茶番）の始まりだじょー 1234!! へっぽこ　
 
 
 		//				int branch = Chaban.prologue(h);
