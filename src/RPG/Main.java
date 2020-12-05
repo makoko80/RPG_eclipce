818a5c73bd9449e6c462e20186be290bba55a672
ryogo <nozaryo8@localhost>
2020-12-05 15:53:40 +0900

    aa

--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -5,15 +5,8 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Hero h = new Hero();
 		Battle b = new Battle();
-		//冒険（茶番）の始まりだじょー 1234!! へっぽこ　
+		DungeonEvent d = new DungeonEvent();
 
-
-		//				int branch = Chaban.prologue(h);
-		//				if (branch == 4) {
-		//					System.out.println("かもね");
-		//					System.exit(0);
-		//				}
-		b.enCount(h);
 
 	}
 
